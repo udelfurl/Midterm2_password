1dd8a78aa5df561022aa262701d595ce45a32aa5
Robert Furlong <stefurlr@udel.edu>
2015-04-28 13:45:52 -0400

    password controller

--- src/ch/makery/view/PasswordController.java
+++ src/ch/makery/view/PasswordController.java
@@ -1,10 +1,24 @@
 package ch.makery.view;
-
+import 
 public class PasswordController {
-	Ok.setOnAction(new EvetnHandler<ActionEvent>(){
+	Ok.setOnAction(new EventHandler<ActionEvent>(){
 	private void handleOK(){
+		if(PasswordField = bugaloo){
+			return display = "Password correct"
+			else{
+				return display = "Password incorrect"
+			}
+		}
 		
 		}
+	Help.setOnAction(new EventHandler<ActionEvent>(){
+		private void handleHelp(){
+			if(Action >= 0){
+				return display = "The Help button was pressed"
+			}
+		}
 	}
+	}
+	
 
 }
